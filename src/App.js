5713783b1554f8fcae01da063f75c1b8c3044fc0
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-12-21 23:51:39 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Boilerplate React + Webpack + Babel, by valentergs</h1>
+        <h3>React: 16.12.0</h3>
+        <h3>Webpack: 4.41.4</h3>
+        <h3>Babel: 7.7.7</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
