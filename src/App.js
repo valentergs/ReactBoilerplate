62ea98be97a981105974dbccc3df18a387afb879
Rodrigo Valente <rodrigovalente@hotmail.com>
2019-12-22 00:10:25 -0300

    use function component on App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Boilerplate React + Webpack + Babel, by valentergs</h1>
-        <h3>React: 16.12.0</h3>
-        <h3>Webpack: 4.41.4</h3>
-        <h3>Babel: 7.7.7</h3>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Boilerplate React + Webpack + Babel, by valentergs</h1>
+      <h3>React: 16.12.0</h3>
+      <h3>Webpack: 4.41.4</h3>
+      <h3>Babel: 7.7.7</h3>
+    </div>
+  );
+};
 
 export default App;
